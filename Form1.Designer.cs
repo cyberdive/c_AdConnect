1a54969b29eb636c6aad0defcfbd98f1d6267e0d
Patrick TELLIER <tellierp@cyberdev.be>
2019-11-05 11:16:42 +0100

    add code for form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -74,6 +74,7 @@ namespace AdConnect
             this.button1.TabIndex = 0;
             this.button1.Text = "Check";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // txtServer
             // 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 
 namespace AdConnect
 {
@@ -20,6 +22,60 @@ namespace AdConnect
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public string PathBase()
+        {
+            return string.Format("LDAP://{0}/{1}", (object)string.Format("{0}:{1}", (object)this.txtServer.Text, (object)this.txtPort.Text), (object)this.txtContainer.Text);
+        }
+
+        private void InitConfiguration()
+        {
+            /// this.environnements = ((IEnumerable<string>)ConfigurationManager.AppSettings["ENVIRONNEMENTS"].Split(',')).ToList<string>();
+            this.lstEnv.DataSource = (object)this.environnements;
+        }
+
+        private void InitForm(string environnement)
+        {
+            ///  this.txtServer.Text = ConfigurationManager.AppSettings[string.Format("{0}_SERVER_HOST", (object)environnement)];
+            ///  this.txtPort.Text = ConfigurationManager.AppSettings[string.Format("{0}_SERVER_PORT", (object)environnement)];
+            ///  this.txtContainer.Text = ConfigurationManager.AppSettings[string.Format("{0}_SERVER_CONTAINER", (object)environnement)];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = this.txtServer.Text;
+                if (string.IsNullOrEmpty(this.txtPort.Text))
+                {
+                    string str = text + ":" + this.txtPort.Text;
+                }
+                /// this.txtOutput.Text = string.Format("{0} results found", (object)new DirectorySearcher(this.CreateDirectoryEntry(this.PathBase()))
+                {
+                    ///    Filter = this.txtFilter.Text
+                }///.FindAll().Count);
+            }
+            catch (Exception ex)
+            {
+                this.txtOutput.Text = ex.ToString() + ex.InnerException?.ToString();
+            }
+        }
+
+        public DirectoryEntry CreateDirectoryEntry(string path)
+        {
+            if (this.chkSSL.Checked)
+                return new DirectoryEntry(path, string.IsNullOrWhiteSpace(this.txtLogin.Text) ? (string)null : this.txtLogin.Text, string.IsNullOrWhiteSpace(this.txtPassword.Text) ? (string)null : this.txtPassword.Text, AuthenticationTypes.Secure | AuthenticationTypes.Encryption);
+            return new DirectoryEntry(path, string.IsNullOrWhiteSpace(this.txtLogin.Text) ? (string)null : this.txtLogin.Text, string.IsNullOrWhiteSpace(this.txtPassword.Text) ? (string)null : this.txtPassword.Text);
+        }
+
+        private void lstEnv_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            this.InitForm(this.lstEnv.SelectedValue.ToString());
         }
     }
 }
