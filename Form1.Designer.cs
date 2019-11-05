167374fa8d5e9125eaaadf56624fc9d69e410657
Patrick TELLIER <tellierp@cyberdev.be>
2019-11-05 11:10:21 +0100

    ajout du layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,21 +37,266 @@ namespace AdConnect
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtContainer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkSSL = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstEnv = new System.Windows.Forms.ComboBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtServer.Location = new System.Drawing.Point(87, 29);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(309, 20);
+            this.txtServer.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtLogin, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtContainer, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.txtServer, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.chkSSL, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtPort, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lstEnv, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPassword, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtFilter, 1, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(32, 11);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49953F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49984F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.4986F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49953F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49953F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.50328F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49984F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.49984F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(399, 215);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 136);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Filter";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Login";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtLogin.Location = new System.Drawing.Point(87, 81);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(309, 20);
+            this.txtLogin.TabIndex = 9;
+            // 
+            // txtContainer
+            // 
+            this.txtContainer.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtContainer.Location = new System.Drawing.Point(87, 159);
+            this.txtContainer.Name = "txtContainer";
+            this.txtContainer.Size = new System.Drawing.Size(309, 20);
+            this.txtContainer.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Root";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // chkSSL
+            // 
+            this.chkSSL.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.chkSSL.AutoSize = true;
+            this.chkSSL.Location = new System.Drawing.Point(87, 190);
+            this.chkSSL.Name = "chkSSL";
+            this.chkSSL.Size = new System.Drawing.Size(46, 17);
+            this.chkSSL.TabIndex = 6;
+            this.chkSSL.Text = "SSL";
+            this.chkSSL.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtPort.Location = new System.Drawing.Point(87, 55);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(309, 20);
+            this.txtPort.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Environnement";
+            // 
+            // lstEnv
+            // 
+            this.lstEnv.FormattingEnabled = true;
+            this.lstEnv.Location = new System.Drawing.Point(87, 3);
+            this.lstEnv.Name = "lstEnv";
+            this.lstEnv.Size = new System.Drawing.Size(309, 21);
+            this.lstEnv.TabIndex = 10;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtPassword.Location = new System.Drawing.Point(87, 107);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(309, 20);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtFilter.Location = new System.Drawing.Point(87, 133);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.PasswordChar = '*';
+            this.txtFilter.Size = new System.Drawing.Size(309, 20);
+            this.txtFilter.TabIndex = 15;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(32, 303);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(707, 242);
+            this.txtOutput.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(924, 573);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ImageList imageList1;
+        private PrincipalContext principalContext;
+        private List<string> environnements;
+        private Button button1;
+        private TextBox txtServer;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox txtContainer;
+        private Label label2;
+        private Label label1;
+        private TextBox txtOutput;
+        private CheckBox chkSSL;
+        private Label label3;
+        private TextBox txtPort;
+        private Label label4;
+        private ComboBox lstEnv;
+        private Label label6;
+        private Label label5;
+        private TextBox txtLogin;
+        private TextBox txtPassword;
+        private Label label7;
+        private TextBox txtFilter;
     }
 }
 
